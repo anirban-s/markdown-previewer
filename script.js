8d6b09dcb8a73e8bde0bf15d6bdef08a76356c83
anirban-s <baban.chrome@gmail.com>
2021-11-19 18:26:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const editor = document.getElementById("editor");
+const preview = document.getElementById("preview");
+
+function displayHtml(event) {
+  let markdownText = event.target.value;
+  display(markdownText);
+}
+
+function display(markdownText) {
+  marked.setOptions({
+    breaks: true });
+
+  preview.innerHTML = marked.parse(markdownText);
+}
+
+function loadInitial() {
+  const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org/), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+**This is boled text**
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+  display(placeholder);
+  editor.innerHTML = placeholder;
+}
+
+editor.addEventListener("keyup", displayHtml);
+
+// On Load
+loadInitial();

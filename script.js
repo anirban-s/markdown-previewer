a8ae1724a62eb972b223b70e0a47e2002c407879
anirban-s <baban.chrome@gmail.com>
2021-11-19 18:30:52 +0530

    image changed

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ And here. | Okay. | I think we get it.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
 
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+![Face Image](https://openclipart.org/image/800px/861)
 `;
   display(placeholder);
   editor.innerHTML = placeholder;
